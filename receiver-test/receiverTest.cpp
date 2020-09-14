cb9176540df117db2bdceddec14a197acf165b63
amutamil <68475039+amutamil@users.noreply.github.com>
2020-09-14 23:07:51 +0530

    Update receiverTest.cpp

--- receiver-test/receiverTest.cpp
+++ receiver-test/receiverTest.cpp
@@ -1,1 +1,13 @@
+#define CATCH_CONFIG_MAIN  
+#include "catch.hpp"
+#include "functions.h"
 
+using namespace functions;
+
+TEST_CASE("remove number from given string") {
+    string str="12testing282number93removal1",str2="testing282anothernumber93removal2";
+    functions::removeNums(str);functions::removeNums(str2)
+    REQUIRE( str  == "testingnumberremoval");
+    REQUIRE( str2 == "testinganothernumberremoval");
+    
+}
