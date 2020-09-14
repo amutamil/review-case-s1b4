c283803c680cdfad8a20c970fda2465ddf202fc3
amutamil <68475039+amutamil@users.noreply.github.com>
2020-09-14 23:13:52 +0530

    Update receiverTest.cpp

--- receiver-test/receiverTest.cpp
+++ receiver-test/receiverTest.cpp
@@ -5,9 +5,8 @@
 using namespace functions;
 
 TEST_CASE("remove number from given string") {
-    string str="12testing282number93removal1",str2="testing282anothernumber93removal2";
+    string str="12testing282 number93 removal1",str2="testing 282another number93 removal2";
     functions::removeNums(str);functions::removeNums(str2);
-    REQUIRE( str  == "testingnumberremoval");
-    REQUIRE( str2 == "testinganothernumberremoval");
-    
+    REQUIRE( str  == "testing number removal");
+    REQUIRE( str2 == "testing another number removal");
 }
