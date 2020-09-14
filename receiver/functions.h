356edbb05681c43c082fa4af0239f9651cf8ce1e
amutamil <68475039+amutamil@users.noreply.github.com>
2020-09-14 14:59:55 +0530

    Update functions.h

--- receiver/functions.h
+++ receiver/functions.h
@@ -87,6 +87,20 @@ void trim(string& s1)
         cout << itr->first
             << '\t' << itr->second << '\n';
       }
+   }
+ 
+ 
+ createCSV(unordered_map<string,int>& m)
+ {
+    ofstream  file("output.csv");
+    file.open("output.csv", ios::out | ios::app);
+
+    unordered_map<string, int> ::iterator it;
+
+    for (it = m.begin(); it != m.end(); ++it) 
+    {
+        file << it->first << "," << it->second << "\n";
+    }
   }
   
 }
