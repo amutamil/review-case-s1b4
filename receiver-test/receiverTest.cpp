0e140e9907de21b6235b52d0d6a02f8f4179aa71
amutamil <68475039+amutamil@users.noreply.github.com>
2020-09-14 23:08:34 +0530

    Update receiverTest.cpp

--- receiver-test/receiverTest.cpp
+++ receiver-test/receiverTest.cpp
@@ -6,7 +6,7 @@ using namespace functions;
 
 TEST_CASE("remove number from given string") {
     string str="12testing282number93removal1",str2="testing282anothernumber93removal2";
-    functions::removeNums(str);functions::removeNums(str2)
+    functions::removeNums(str);functions::removeNums(str2);
     REQUIRE( str  == "testingnumberremoval");
     REQUIRE( str2 == "testinganothernumberremoval");
     
