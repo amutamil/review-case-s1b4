7c7a728090fa537fe17c73744e7670c2845861e8
HarishMurugancoolz <48434581+HarishMurugancoolz@users.noreply.github.com>
2020-09-15 08:59:17 +0530

    Add files via upload

---
+++ sender-test/fileArray.h
@@ -0,0 +1,72 @@
+
+#include<vector>
+#include<string>
+#include<sstream>
+#include<iostream>
+using namespace std;
+namespace FileArray
+{
+	class FileArrayCreator
+	{
+	public:
+		virtual vector<vector<string>> getFileArray() = 0;
+	};
+	class CSVFileArrayCreator:public FileArrayCreator
+	{
+	private:
+		vector<string> row;
+		vector<vector<string>> csvFileArray;
+		istream& fin;
+	public:
+		CSVFileArrayCreator(istream& f):fin(f){}
+		bool isLineNotEmpty(string line)
+		{
+			return(!(line == ","||line==""));
+		}
+		bool isLineNotContainComma(string line)
+		{
+			for (unsigned int i = 0; i < line.length(); i++)
+			{
+				if (line[i] == ',')
+					return false;
+
+			}
+			return true;
+		}
+		
+		void pushWordsToRowVector(vector<string>& row, string line)
+		{
+			if (isLineNotContainComma(line))
+				row.push_back("");
+			stringstream s(line);
+			while (s.good())
+			{
+				string substr;
+				getline(s, substr, ',');
+				row.push_back(substr);
+			}
+		}
+		vector<vector<string>> getFileArray()
+		{
+			string line;
+			vector<string> row;
+			while (getline(fin, line, '\n'))
+			{
+				if (isLineNotEmpty(line))
+				{
+					pushWordsToRowVector(row,line);
+					csvFileArray.push_back(row);
+					row.clear();
+				}
+
+			}
+			return csvFileArray;
+		}
+		
+		
+	};
+	bool IsArgumentCountEqaulToOne(int argc);
+	bool IsFileArrayIsEmpty(vector<vector<string>> csvFileArray);
+	bool IsFileNotOpen(ifstream& fin,string fileName);
+	void printReview(vector<vector<string>> csvFileArray);
+}
