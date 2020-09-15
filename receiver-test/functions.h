fbe362d56765c2f531748689a94603091b1e30ca
amutamil <68475039+amutamil@users.noreply.github.com>
2020-09-15 17:46:30 +0530

    Update functions.h

--- receiver-test/functions.h
+++ receiver-test/functions.h
@@ -81,15 +81,14 @@ void trim(string& s1)
   
   void printMapAndCreateCSV(unordered_map<string, int>& m)
   {	
-	ofstream  file("output.csv");
-    	file.open("output.csv", ios::out | ios::app);
-	  
-   	unordered_map<string, int>::iterator itr;
-   	for (auto itr = m.begin(); itr != m.end(); ++itr) 
-      	{
+	ofstream  file;
+    	file.open("output.csv", ios::out);
+    	unordered_map<string, int>::iterator itr;
+    	for (auto itr = m.begin(); itr != m.end(); ++itr)
+    	{
         	cout << itr->first << "\t\t\t" << itr->second << '\n';
-		file << itr->first << "," << itr->second << "\n";
-     	}
-   }
+        	file << itr->first << "," << itr->second << "\n";
+    	}
+  }
  
 }
