b7d8a69e67c8f377cbd785888f011a396933e304
HarishMurugancoolz <48434581+HarishMurugancoolz@users.noreply.github.com>
2020-09-15 18:29:31 +0530

    Update ColumnFilter.cpp

--- sender/ColumnFilter.cpp
+++ sender/ColumnFilter.cpp
@@ -8,15 +8,36 @@ void ColumnFilter::printColumnReview(vector<string> ColumnReview)
 	vector<string>::iterator string_itr = ColumnReview.begin();
 	while (string_itr != ColumnReview.end())
 	{
-		cout << *string_itr;
-		cout << endl;
+		cout << *string_itr<< endl;
 		string_itr++;
 	}
 }
-bool ColumnFilter::IsColumnNumberWithinLimit(vector <vector<string>> csvFileArray,int colNum)
+bool ColumnFilter::IsStringIsValidForStoi(string str)
+{
+	try
+	{
+		stoi(str);
+	}
+	catch (...)
+	{
+		return false;
+	}
+	return true;
+}
+bool ColumnFilter::IsColumnNumberWithinLimit(vector <vector<string>> csvFileArray, int colNum)
 {
 	
-	return(colNum>0&&colNum<=(int)csvFileArray[0].size());
+		return(colNum > 0 && colNum <= (int)csvFileArray[0].size());
+}
+bool ColumnFilter::IsColumnNumberCorrect(vector <vector<string>> csvFileArray,string colNum)
+{
+	
+	if (IsStringIsValidForStoi(colNum))
+	{
+		
+		return(IsColumnNumberWithinLimit(csvFileArray,stoi(colNum)));
+	}
+	return false;
 }
 
 
